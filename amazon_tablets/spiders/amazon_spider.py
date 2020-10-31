a6c178c6ead402aeba987225d512ce9b05819e15
Shalabh Agarwala <shalabh.agarwala@gmail.com>
2020-10-31 04:14:42 -0700

    updated items and spider

--- amazon_tablets/spiders/amazon_spider.py
+++ amazon_tablets/spiders/amazon_spider.py
@@ -5,7 +5,7 @@ class AmazonSpider(scrapy.Spider):
     name = 'amazon_spider'
     
      # How many pages you want to scrape
-    no_of_pages = 10
+    no_of_pages = 1
 
     # Headers to fix 503 service unavailable error
     # Spoof headers to force servers to think that request coming from browser ;)
@@ -16,5 +16,30 @@ class AmazonSpider(scrapy.Spider):
         urls = ["https://www.amazon.com/s?k=tablet&i=electronics&ref=nb_sb_noss_1"]
 
         for url in urls: yield scrapy.Request(url = url, callback = self.parse, headers = self.headers)
+
+    def parse(self, response):
+
+    	self.no_of_pages -= 1
+
+    	tablets = response.xpath("//a[@class='a-link-normal a-text-normal']").xpath("@href").getall()
+
+    	# print(len(tablets))
+
+        for tablet in tablets:
+            final_url = response.urljoin(tablet)
+            yield scrapy.Request(url=final_url, callback = self.parse_mobile, headers = self.headers)
+            # break
+            # print(final_url)
+
+        # print(response.body)
+        # title = response.xpath("//span[@class='a-size-medium a-color-base a-text-normal']//text()").getall()
+        # title = response.css('span').getall()
+        # print(title)
+        
+        if(self.no_of_pages > 0):
+            next_page_url = response.xpath("//ul[@class='a-pagination']/li[@class='a-last']/a").xpath("@href").get()
+            final_url = response.urljoin(next_page_url)
+            yield scrapy.Request(url = final_url, callback = self.parse, headers = self.headers)
+
 
    
